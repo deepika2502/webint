cfdf583066a0a3d3c3db63b0cd9f38d982246674
deepika2502 <46070106+deepika2502@users.noreply.github.com>
2022-01-23 19:25:55 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function reset_waste() {
     localStorage.getItem("travel_slider")
   );
 
-  document.getElementById("disabledRange").value =localStorage.getItem("waste_slider") || 50;
+  document.getElementById("disabledRange").value =
+    localStorage.getItem("waste_slider") || 50;
   slider_waste = localStorage.getItem("waste_slider") || 1;
   document.getElementById("organic").value = "";
   document.getElementById("glass").value = "";
@@ -33,8 +34,8 @@ function reset_travel() {
   );
 
   document.getElementById("disabledRange").value =
-    localStorage.getItem("travel_slider") || 1;
-  slider_travel = localStorage.getItem("travel_slider") || 1;
+    localStorage.getItem("travel_slider") || 50;
+  slider_travel = localStorage.getItem("travel_slider") || 50;
   document.getElementById("bike").value = "";
   document.getElementById("train").value = "";
   document.getElementById("car").value = "";
@@ -50,8 +51,8 @@ function reset_electricity() {
   );
 
   document.getElementById("disabledRange").value =
-    localStorage.getItem("electricity_slider") || 1;
-  slider_electricity = localStorage.getItem("electricity_slider") || 1;
+    localStorage.getItem("electricity_slider") || 50;
+  slider_electricity = localStorage.getItem("electricity_slider") || 50;
   document.getElementById("led").value = "";
   document.getElementById("bulb").value = "";
   document.getElementById("heater").value = "";
@@ -106,13 +107,13 @@ function recycle(id) {
       y = parseInt(document.getElementById("glass").value);
       break;
     case "glass_2":
-      x = parseInt(document.getElementById("glass").value);    
+      x = parseInt(document.getElementById("glass").value);
       break;
     case "clothes_1":
-      y = parseInt(document.getElementById("clothes").value);       
+      y = parseInt(document.getElementById("clothes").value);
       break;
     case "clothes_2":
-      x = parseInt(document.getElementById("clothes").value);      
+      x = parseInt(document.getElementById("clothes").value);
       break;
     case "aluminium_1":
       y = parseInt(document.getElementById("aluminium").value);
@@ -121,19 +122,26 @@ function recycle(id) {
       x = parseInt(document.getElementById("aluminium").value);
       break;
   }
-  totalMinW = totalMinW + x + y;
-  var percentGreen = (x / totalMinW) * 100;
-  var percentCarbon = (y / totalMinW) * 100;
-  totalW = totalW + (percentGreen - percentCarbon) / 2;
+  if (isNaN(x) || isNaN(y)) {
+    window.alert("Enter a valid number");
+  } else {
+    totalMinW = totalMinW + x + y;
+    var percentGreen = (x / totalMinW) * 100;
+    var percentCarbon = (y / totalMinW) * 100;
+    totalW = totalW + (percentGreen - percentCarbon) / 2;
 
-  totalW = totalW > 100 ? 100 : totalW;
-  totalW = totalW < 0  ? 0: totalW;
-  console.log(x, y, totalW);
-  document.getElementById("disabledRange").value = totalW;
-  localStorage.setItem(
-    "waste_slider",
-    document.getElementById("disabledRange").value
-  );
+    totalW = totalW > 100 ? 100 : totalW;
+    totalW = totalW < 0 ? 0 : totalW;
+    console.log(x, y, totalW);
+    document.getElementById("disabledRange").value = totalW;
+    document.getElementById("organic").value = "";
+    document.getElementById("glass").value = "";
+    document.getElementById("plastic").value = "";
+    document.getElementById("battery").value = "";
+    document.getElementById("clothes").value = "";
+    document.getElementById("aluminium").value = "";
+    localStorage.setItem("waste_slider", totalW);
+  }
 }
 
 function travel(id) {
@@ -145,46 +153,54 @@ function travel(id) {
   switch (id) {
     case "bus_b":
       x = 2 * parseInt(document.getElementById("bus").value);
-      
+
       break;
 
     case "train_b":
       x = 2 * parseInt(document.getElementById("train").value);
-      
+
       break;
 
     case "walk_b":
       x = 5 * parseInt(document.getElementById("walk").value);
-      
+
       break;
     case "bike_b":
       x = 3 * parseInt(document.getElementById("bike").value);
-      
+
       break;
     case "car_b":
       y = parseInt(document.getElementById("car").value);
-       
+
       break;
     case "taxi_b":
       y = parseInt(document.getElementById("taxi").value);
-       
+
       break;
   }
+  if (isNaN(x) || isNaN(y)) {
+    window.alert("Enter a valid number");
+  } else {
+    totalMinT = totalMinT + x + y;
+    var percentGreen = (x / totalMinT) * 100;
+    var percentCarbon = (y / totalMinT) * 100;
+    totalT = totalT + (percentGreen - percentCarbon) / 2;
 
-  totalMinT = totalMinT + x + y;
-  var percentGreen = (x / totalMinT) * 100;
-  var percentCarbon = (y / totalMinT) * 100;
-  totalT = totalT + (percentGreen - percentCarbon) / 2;
-
-  totalT = totalT > 100 ?  100: totalT;
-  totalT = totalT < 0 ? 0 :totalT;
-  console.log(totalT); //the value to put on slider
-  document.getElementById("disabledRange").value = totalT;
-
-  localStorage.setItem(
-    "travel_slider",
-    document.getElementById("disabledRange").value
-  );
+    totalT = totalT > 100 ? 100 : totalT;
+    totalT = totalT < 0 ? 0 : totalT;
+    console.log(totalT); //the value to put on slider
+    document.getElementById("disabledRange").value = totalT;
+    document.getElementById("bike").value = "";
+    document.getElementById("train").value = "";
+    document.getElementById("car").value = "";
+    document.getElementById("taxi").value = "";
+    document.getElementById("walk").value = "";
+    document.getElementById("bus").value = "";
+    localStorage.setItem(
+      "travel_slider",
+      document.getElementById("disabledRange").value
+    );
+  }
 }
 
 function electricity(id) {
@@ -195,44 +211,54 @@ function electricity(id) {
   switch (id) {
     case "daylight_b":
       x = 3 * parseInt(document.getElementById("daylight").value);
-       
+
       break;
 
     case "led_b":
-      x = 3* parseInt(document.getElementById("led").value);
-       
+      x = 3 * parseInt(document.getElementById("led").value);
+
       break;
 
     case "bulb_b":
       y = parseInt(document.getElementById("bulb").value);
-       
+
       break;
     case "wash_b":
       y = parseInt(document.getElementById("wash").value);
-       
+
       break;
     case "heater_b":
       y = parseInt(document.getElementById("heater").value);
-       
+
       break;
     case "dishwasher_b":
       y = parseInt(document.getElementById("dishwasher").value);
-       
+
       break;
   }
 
-  totalMinE = totalMinE + x + y;
-  var percentGreen = (x / totalMinE) * 100;
-  var percentCarbon = (y / totalMinE) * 100;
-  totalE = totalE + (percentGreen - percentCarbon) / 2;
+  if (isNaN(x) || isNaN(y)) {
+    window.alert("Enter a valid number");
+  } else {
+    totalMinE = totalMinE + x + y;
+    var percentGreen = (x / totalMinE) * 100;
+    var percentCarbon = (y / totalMinE) * 100;
+    totalE = totalE + (percentGreen - percentCarbon) / 2;
 
-  totalE = totalE > 100?  100: totalE;
-  totalE = totalE < 0? 0 : totalE;
-  document.getElementById("disabledRange").value = totalE;
-  console.log(totalE);
+    totalE = totalE > 100 ? 100 : totalE;
+    totalE = totalE < 0 ? 0 : totalE;
+    document.getElementById("disabledRange").value = totalE;
+    document.getElementById("led").value = "";
+    document.getElementById("bulb").value = "";
+    document.getElementById("heater").value = "";
+    document.getElementById("daylight").value = "";
+    document.getElementById("wash").value = "";
+    document.getElementById("dishwasher").value = "";
+    console.log(totalE);
 
-  localStorage.setItem(
-    "electricity_slider",
-    document.getElementById("disabledRange").value
-  );
+    localStorage.setItem(
+      "electricity_slider",
+      document.getElementById("disabledRange").value
+    );
+  }
 }
